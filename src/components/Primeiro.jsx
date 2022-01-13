9e92e959e71328883ad98ee16fa6ee3ea38b2bca
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2022-01-13 14:17:08 -0300

    Criar primeiro componente

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Primeiro (){
+    return(
+        <div>
+            <h1>Primeiro</h1>
+            <h2>Segundo</h2>
+        </div>
+    )
+}
+export default Primeiro
