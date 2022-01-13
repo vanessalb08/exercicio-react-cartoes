a1ae4cb4448f81e132ee005b39d862b35410ee3f
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2022-01-13 14:18:45 -0300

    Inserir modificaçõs aon index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './components/layout/Card';
+import Primeiro from './components/Primeiro';
+//import Card1 from './components/layout/Card1';
 
 ReactDOM.render(
-  <React.StrictMode>
-  
-  </React.StrictMode>,
+  <div>
+    <Card titulo ="Exercicio">
+      Conteudo
+    </Card>
+    <Card titulo ="Exercicio">
+      <Primeiro/>
+    </Card>
+  </div>,
   document.getElementById('root')
 );
 
